c65398e30ecca43e1bbc0029b4d34313899dcb40
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-06-23 18:42:12 +0100

    Materials can be created

--- src/pages/SubtopicAdd/SubtopicAdd.jsx
+++ src/pages/SubtopicAdd/SubtopicAdd.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import './SubtopicAdd.css'
 
-// import modules from '../utils/quilModules'
+import modules from '../../utils/quilModules'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -77,6 +77,65 @@ const SubtopicAdd = () => {
     return (
         <div className='subtopic-add-container'>
             <h1>Add Study Material</h1>
+
+            <div className="section-form">
+          <div className="horizontal-container cover-container">
+            <p>Cover</p>
+            <input 
+                className='section-cover-input'
+                type='file' 
+                accept='image/*' 
+                key={inputKey} 
+                value={undefined} 
+                onChange={(e)=> setCover(e.target.files[0])}
+            />
+
+            <button
+              className='clear-img-btn' 
+              onClick={clearImage}
+              >Clear</button>
+          </div>
+
+          <input
+              className='section-title-input'
+              type='text'
+              name='title'
+              placeholder='Title...'
+              value={subtopic.title}
+              onChange={handleInputChange}
+          />
+          <input
+              className='section-subtitle-input'
+              type='text'
+              name='subtitle'
+              placeholder='Subitle...'
+              value={subtopic.subtitle}
+              onChange={handleInputChange}
+          />
+
+
+          <ReactQuill 
+            className='editor-input'
+            modules={modules}
+            theme="snow" 
+            value={subtopic.body} 
+            placeholder='Type here...'
+            onChange={body => handleInputChange({ target: { value: body, name: 'body' } })}
+        />
+        
+          <button 
+            className='section-add-btn'
+            onClick={handleSubmit}>
+              Done
+          </button>
+
+          <button 
+            className='section-cancel-btn'
+            onClick={cancel}
+          >
+            Cancel
+          </button>
+        </div>
 
         </div>
     )
