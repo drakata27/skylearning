ce240bb75dd6a13ee2a60ff1f1ebd662e5d768a3
drakata27 <theall3xander@gmail.com>
2024-08-13 13:32:40 +0100

    Updated assets

--- src/pages/Unauthorized/Unauthorized.jsx
+++ src/pages/Unauthorized/Unauthorized.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import UnauthImg from '../../assets/unauth.webp'
 import './Unauthorized.css'
 
 const Unauthorized = () => {
@@ -8,7 +7,7 @@ const Unauthorized = () => {
         <h1>Unauthorized</h1>
         <div className='unauth-content'>
             <p>You do not have access to view this page</p>
-            <img src={UnauthImg} alt="unauthorized" />
+            <img src={'https://cloud-learn-bucket.s3.eu-west-2.amazonaws.com/assets/unauth.webp'} alt="unauthorized" />
         </div>
     </div>
   )
