db6e3e093505295c2d6521ab8c50c91d8c287cbf
drakata27 <theall3xander@gmail.com>
2024-10-19 19:36:04 +0100

    Added unit tests for other components

---
+++ src/Components/FlashCardItem/FlashCardItem.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+import  FlashCardItem  from './FlashCardItem';
+
+describe('FlashCardItem', ()=>{
+    test('renders FlashCardItem correctly', () => {
+        const card = { id: 1, question: 'Test Question?', answer: 'Test Answer' };
+        const refreshCard = jest.fn();
+    
+        render(
+            <MemoryRouter>
+                <FlashCardItem card={card} refreshCard={refreshCard} />
+            </MemoryRouter>
+        );
+    
+        expect(screen.getByText(/Test Question\?/i)).toBeInTheDocument();
+        expect(screen.getByText(/Test Answer/i)).toBeInTheDocument();
+    });
+})
